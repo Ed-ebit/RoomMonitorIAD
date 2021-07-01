132894217863d23928d41417c079a4e6a741d173
KMV <karl-lost@gmx.de>
2021-07-01 14:56:36 +0200

    2nd

---
+++ src/de/iad/ef/model/RollOverCounter.java
@@ -0,0 +1,23 @@
+package de.iad.ef.model;
+
+public class RollOverCounter extends LimitedCounter {
+
+    /*Methoden*/
+
+    @Override{
+    public void count(){
+    }
+    }
+    public RollOverCounter(){
+        this.setCount(minimumIs());
+
+    }
+    public void RollOverCounter(Integer, Integer){
+
+    }
+    public void unCount(){
+
+    }
+
+
+}
